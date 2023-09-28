7a5d11b844fa0304c1c2bf3430b8bfd8e1a4bf0f
Jacob H. Rothschild <jacob.r.developer@gmail.com>
2023-09-28 20:14:19 +0200

    Input prompting of Whisper

--- main.py
+++ main.py
@@ -25,9 +25,20 @@ def setup_dir(dirpath: Path):
     dirpath.mkdir()
 
 
-def transcribe(fname):
-    with open(fname, "rb") as f:
-        return openai.Audio.transcribe("whisper-1", f)["text"]
+def transcribe(fpath, messages: list[dict]):
+    # This prompt is fed to Whisper. Whisper will then try to match its style. Whisper ignores all but the
+    # last 244 tokens, which is why the most important prompts are at the end.
+    input_prompt = "\n".join(
+        [message["content"] for message in messages[1:-1]]  # All but the system message and the last assistant message
+    ) + (
+        messages[0]["content"] + "\n" +  # The system message
+        messages[-1]["content"] + "\n" +  # The assistant's last message
+        "Hello, welcome to my lecture.\n" +  # To include punctuation
+        "Umm, let me think like, hmm... Okay, here's what I'm, like, thinking."  # To include filler words
+    )
+    print(input_prompt)
+    with open(fpath, "rb") as f:
+        return openai.Audio.transcribe("whisper-1", f, prompt=input_prompt, language="en")["text"]
 
 
 def speak(text: str, fname):
@@ -49,9 +60,9 @@ tmpdir = Path(".tmp/")
 tmpfile = tmpdir / "audio.wav"
 recorder = Recorder()
 while True:
-    setup_dir(tmpdir)
-    recorder.record(tmpfile)
-    prompt = transcribe(tmpfile)
+    # setup_dir(tmpdir)
+    # recorder.record(tmpfile)
+    prompt = transcribe(tmpfile, messages)
     print(f"You: {prompt}")
     print('#' * 80)
     messages.append({"role": "user", "content": prompt})
