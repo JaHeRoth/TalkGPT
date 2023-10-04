9065d0c6c3dd0b462d67964e6148fad1dd56a45c
Jacob H. Rothschild <jacob.r.developer@gmail.com>
2023-10-04 19:26:33 +0200

    Migrated transcription to separate file and class

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import soundfile as sf
 from gtts import gTTS
 
 from recorder import Recorder
+from transcriber import transcribe, Transcriber
+
 
 # TODO: Write README and LICENSE
 # TODO?: Handle long recordings (split of new file every 30 seconds or so, launching async transcribing,
@@ -22,36 +24,6 @@ def setup_dir(dirpath: Path):
     if dirpath.exists():
         shutil.rmtree(dirpath)
     dirpath.mkdir()
-
-
-# TODO: Split this function into multiple functions
-def transcribe(fpath, messages: list[dict]):
-    # This prompt is fed to Whisper. Whisper will then try to match its style. Whisper ignores all but the
-    # last 244 tokens, which is why the most important prompts are at the end.
-    input_prompt = "\n".join(
-        [message["content"] for message in messages[1:-1]]  # All but the system message and the last assistant message
-    ) + (
-        messages[0]["content"] + "\n" +  # The system message
-        messages[-1]["content"] + "\n" +  # The assistant's last message
-        "Hello, welcome to my lecture.\n" +  # To include punctuation
-        "Umm, let me think like, hmm... Okay, here's what I'm, like, thinking."  # To include filler words
-        # TODO: Also include last words of the last part of transcription, if split into multiple files
-    )
-    with open(fpath, "rb") as f:
-        raw_transcription = openai.Audio.transcribe("whisper-1", f, prompt=input_prompt, language="en")["text"]
-
-    # Post-processing with GPT-4
-    post_processing_system = "You are a helpful assistant. Your task is to correct any spelling discrepancies in " \
-                             "the transcribed text. Only add necessary punctuation such as periods, commas, and " \
-                             "capitalization, and use only the context provided."
-    post_processing_messages = (
-            [{"role": "system", "content": post_processing_system}]
-            + messages[1:]
-            + [{"role": "user", "content": raw_transcription}]
-    )
-    return openai.ChatCompletion.create(
-        model="gpt-4", messages=post_processing_messages
-    )["choices"][0]["message"]["content"]
 
 
 def speak(text: str, fname):
@@ -72,10 +44,11 @@ messages = [{
 tmpdir = Path(".tmp/")
 tmpfile = tmpdir / "audio.mp3"
 recorder = Recorder()
+transcriber = Transcriber()
 while True:
     setup_dir(tmpdir)
     recorder.record(tmpfile)
-    prompt = transcribe(tmpfile, messages)
+    prompt = transcriber.transcribe(tmpfile, messages)
     print(f"You: {prompt}")
     print('#' * 80)
     messages.append({"role": "user", "content": prompt})
---
+++ transcriber.py
@@ -0,0 +1,32 @@
+import openai
+
+
+class Transcriber:
+    # TODO: Split this function into multiple functions
+    def transcribe(self, fpath, messages: list[dict]):
+        # This prompt is fed to Whisper. Whisper will then try to match its style. Whisper ignores all but the
+        # last 244 tokens, which is why the most important prompts are at the end.
+        input_prompt = "\n".join(
+            [message["content"] for message in messages[1:-1]]  # All but the system message and the last assistant message
+        ) + (
+            messages[0]["content"] + "\n" +  # The system message
+            messages[-1]["content"] + "\n" +  # The assistant's last message
+            "Hello, welcome to my lecture.\n" +  # To include punctuation
+            "Umm, let me think like, hmm... Okay, here's what I'm, like, thinking."  # To include filler words
+            # TODO: Also include last words of the last part of transcription, if split into multiple files
+        )
+        with open(fpath, "rb") as f:
+            raw_transcription = openai.Audio.transcribe("whisper-1", f, prompt=input_prompt, language="en")["text"]
+
+        # Post-processing with GPT-4
+        post_processing_system = "You are a helpful assistant. Your task is to correct any spelling discrepancies in " \
+                                 "the transcribed text. Only add necessary punctuation such as periods, commas, and " \
+                                 "capitalization, and use only the context provided."
+        post_processing_messages = (
+                [{"role": "system", "content": post_processing_system}]
+                + messages[1:]
+                + [{"role": "user", "content": raw_transcription}]
+        )
+        return openai.ChatCompletion.create(
+            model="gpt-4", messages=post_processing_messages
+        )["choices"][0]["message"]["content"]
