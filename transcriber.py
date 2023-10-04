349d87e57aac9dfc6be11f3ef0ade93160fe7479
Jacob H. Rothschild <jacob.r.developer@gmail.com>
2023-10-04 19:30:30 +0200

    Split transcription into multiple methods

--- transcriber.py
+++ transcriber.py
@@ -2,22 +2,26 @@ import openai
 
 
 class Transcriber:
-    # TODO: Split this function into multiple functions
     def transcribe(self, fpath, messages: list[dict]):
+        return self.post_process(self.initial_transcribe(fpath, messages), messages)
+
+    def initial_transcribe(self, fpath, messages: list[dict]):
         # This prompt is fed to Whisper. Whisper will then try to match its style. Whisper ignores all but the
         # last 244 tokens, which is why the most important prompts are at the end.
         input_prompt = "\n".join(
-            [message["content"] for message in messages[1:-1]]  # All but the system message and the last assistant message
+            [message["content"] for message in messages[1:-1]]
+            # All but the system message and the last assistant message
         ) + (
-            messages[0]["content"] + "\n" +  # The system message
-            messages[-1]["content"] + "\n" +  # The assistant's last message
-            "Hello, welcome to my lecture.\n" +  # To include punctuation
-            "Umm, let me think like, hmm... Okay, here's what I'm, like, thinking."  # To include filler words
-            # TODO: Also include last words of the last part of transcription, if split into multiple files
+                messages[0]["content"] + "\n" +  # The system message
+                messages[-1]["content"] + "\n" +  # The assistant's last message
+                "Hello, welcome to my lecture.\n" +  # To include punctuation
+                "Umm, let me think like, hmm... Okay, here's what I'm, like, thinking."  # To include filler words
+                # TODO: Also include last words of the last part of transcription, if split into multiple files
         )
         with open(fpath, "rb") as f:
-            raw_transcription = openai.Audio.transcribe("whisper-1", f, prompt=input_prompt, language="en")["text"]
+            return openai.Audio.transcribe("whisper-1", f, prompt=input_prompt, language="en")["text"]
 
+    def post_process(self, raw_transcription, messages: list[dict]):
         # Post-processing with GPT-4
         post_processing_system = "You are a helpful assistant. Your task is to correct any spelling discrepancies in " \
                                  "the transcribed text. Only add necessary punctuation such as periods, commas, and " \
