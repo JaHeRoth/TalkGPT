9f2a4204ac98a95e3f8401001bc2c45d5dea95b8
Jacob H. Rothschild <jacob.r.developer@gmail.com>
2023-10-05 20:46:17 +0200

    Support for local transcription

--- main.py
+++ main.py
@@ -5,17 +5,14 @@ from chat_bot import ChatBot, OpenAIResponder, HuggingFaceResponder
 from logger import Logger, Verbosity
 from recorder import Recorder
 from speaker import Speaker
-from transcriber import Transcriber
+from transcriber import Transcriber, OpenAIASR, HuggingFaceASR
 
 
 # TODO?: Refactor (maybe LangChain can help reduce messiness)
 # TODO: Write README and LICENSE
-# TODO?: Handle long recordings (split of new file every 30 seconds or so, launching async transcribing,
-#  before combining everything at the end)
-#  https://platform.openai.com/docs/guides/speech-to-text/longer-inputs
 # TODO: Support adding PDFs to context
 # TODO?: Streamlit
-# TODO?: Improve text to speech
+# TODO: Improve text to speech (support for abritrary Transformers model)
 # TODO?: Generate avatars from speech (to make it as if you're talking to a person)
 
 
@@ -27,10 +24,11 @@ def setup_dir(dirpath: Path):
 
 tmpdir = Path(".tmp/")
 tmpfile = tmpdir / "audio.mp3"
+asr = HuggingFaceASR()
 responder = HuggingFaceResponder()
 logger = Logger(verbosity=Verbosity.NORMAL)
 recorder = Recorder(logger)
-transcriber = Transcriber(responder=None)
+transcriber = Transcriber(asr, responder=None)
 speaker = Speaker(logger)
 print("What kind of chatbot should I be? Please write your answer and press Enter:")
 bot = ChatBot(responder, system_message=input())
--- transcriber.py
+++ transcriber.py
@@ -1,10 +1,35 @@
 import openai
+from transformers import pipeline
 
 from chat_bot import ChatBot, Responder
 
 
+class ASR:
+    def __call__(self, fpath, prompt: str):
+        raise NotImplementedError
+
+
+class OpenAIASR(ASR):
+    def __init__(self, model_name="whisper-1"):
+        self.model_name = model_name
+
+    def __call__(self, fpath, prompt: str):
+        with open(fpath, "rb") as file:
+            return openai.Audio.transcribe(self.model_name, file, prompt=prompt, language="en")["text"]
+
+
+class HuggingFaceASR(ASR):
+    def __init__(self, model_name="openai/whisper-large-v2"):
+        self.model_name = model_name
+        self.asr = pipeline("automatic-speech-recognition", model=self.model_name)
+
+    def __call__(self, fpath, prompt: str):
+        return self.asr(str(fpath))["text"]
+
+
 class Transcriber:
-    def __init__(self, responder: Responder|None):
+    def __init__(self, asr: ASR, responder: Responder | None):
+        self.asr = asr
         self.responder = responder
 
     def transcribe(self, fpath, messages: list[dict]):
@@ -21,10 +46,8 @@ class Transcriber:
                 messages[-1]["content"] + "\n" +  # The assistant's last message
                 "Hello, welcome to my lecture.\n" +  # To include punctuation
                 "Umm, let me think like, hmm... Okay, here's what I'm, like, thinking."  # To include filler words
-                # TODO: Also include last words of the last part of transcription, if split into multiple files
         )
-        with open(fpath, "rb") as f:
-            return openai.Audio.transcribe("whisper-1", f, prompt=input_prompt, language="en")["text"]
+        return self.asr(fpath, input_prompt)
 
     def post_process(self, raw_transcription, messages: list[dict]):
         if self.responder is None:
